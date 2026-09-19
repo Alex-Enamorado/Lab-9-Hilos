c64ef54c9d5a81b4690a136135e1c1ea5f543056
Alex-Enamorado <alex_enamorado@unitec.edu>
2026-09-19 09:03:37 -0600

    Feat: Lista enlazada y nodos

---
+++ Lab9/src/lab9/estructuras/ListaEnlazada.java
@@ -0,0 +1,66 @@
+package lab9.estructuras;
+
+public class ListaEnlazada<T> {
+
+    private Nodo<T> cabeza;
+    private int tamano;
+
+    public ListaEnlazada() {
+        this.cabeza = null;
+        this.tamano = 0;
+    }
+
+    public synchronized void agregar(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo<T> actual = cabeza;
+            while (actual.getSiguiente() != null) {
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nuevo);
+        }
+        tamano++;
+        notifyAll();
+    }
+
+    public synchronized int tamano() {
+        return tamano;
+    }
+
+    public synchronized boolean estaVacia() {
+        return tamano == 0;
+    }
+
+    public synchronized boolean eliminar(T dato) {
+        Nodo<T> actual = cabeza;
+        Nodo<T> anterior = null;
+        while (actual != null) {
+            if (actual.getDato().equals(dato)) {
+                if (anterior == null) {
+                    cabeza = actual.getSiguiente();
+                } else {
+                    anterior.setSiguiente(actual.getSiguiente());
+                }
+                tamano--;
+                notifyAll();
+                return true;
+            }
+            anterior = actual;
+            actual = actual.getSiguiente();
+        }
+        return false;
+    }
+
+    public synchronized T obtener(int indice) {
+        if (indice < 0 || indice >= tamano) {
+            return null;
+        }
+        Nodo<T> actual = cabeza;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.getSiguiente();
+        }
+        return actual.getDato();
+    }
+}
